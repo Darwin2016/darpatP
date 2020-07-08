ab918cf036c9db2f6919b99858c46e6acb611eac
Darwin <dpparies@gmail.com>
2020-07-08 02:15:19 -0500

    creacion de HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,14 @@
+/*
+   Primer programa en Java: HolaMundo.java
+   darpatX
+   julio 2020
+*/
+public class HolaMundo
+{
+  public static void main(String[] args) 
+   {
+       
+    System.out.print("Hola Mundo ...!");
+       
+   } // fin del main 
+}//fin de la clase
